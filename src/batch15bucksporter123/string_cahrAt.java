be02bc21cdfcc65a1f7454cd77c3277060940b55
Sarika <sarikaparandkar29@gmail.com>
2022-11-21 13:06:20 +0530

    firstCommit

---
+++ src/batch15bucksporter123/string_cahrAt.java
@@ -0,0 +1,16 @@
+package batch15bucksporter123;
+
+public class string_cahrAt {
+
+	public static void main(String[] args) {
+		
+		// this method use find character
+		String a="shinchan";
+		
+		System.out.println(a.charAt(4));  // h 
+		
+		System.out.println("this is Sarika");
+
+	}
+
+}
